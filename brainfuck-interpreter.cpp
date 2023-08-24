1b628e7e6d818cae22fd8a90b62886ac7dbd8b56
Ali Atashrooz <atashroozarash@gmail.com>
2023-08-24 16:29:27 +0330

    Add program loop

--- brainfuck-interpreter.cpp
+++ brainfuck-interpreter.cpp
@@ -22,86 +22,94 @@ using namespace std;
 
 int main()
 {
-    vector<int> tape(30000, 0); // brainfuck tape with 30000 cells
-    int ptr = 0;                // pointer to the current cell (memory address)
-    int code_ptr = 0;           // pointer to current code instruction
-
-    cout << "> ";
-    
-    string code;
-    getline(cin, code);
-
-    while (code_ptr < code.size())
+    while (true)
     {
-        char instruction = code[code_ptr];
+        vector<int> tape(30000, 0); // brainfuck tape with 30000 cells
+        int ptr = 0;                // pointer to the current cell (memory address)
+        int code_ptr = 0;           // pointer to current code instruction
 
-        // brainfuck instructions
-        // https://en.wikipedia.org/wiki/Brainfuck#Language_design
-        switch (instruction)
-        {
-        case '>':   // increment cell pointer
-            ++ptr;
-            break;
-
-        case '<':   // decrement cell pointer
-            --ptr;
-            break;
-
-        case '+':   // increment byte at ptr cell
-            ++tape[ptr];
-            break;
-
-        case '-':   // decrement byte at ptr cell
-            --tape[ptr];
-            break;
-
-        case '.':   // print byte at ptr cell
-            cout << (char)tape[ptr];
-            break;
-
-        case ',':   // accept one byte of input, store it's value at ptr cell
-            tape[ptr] = cin.get();
-            break;
-
-        case '[':   // start a loop
-            if (tape[ptr] == 0)
-            {
-                int loop_count = 1;
-                while (loop_count > 0)
-                {
-                    ++code_ptr;
-                    if (code[code_ptr] == '[')
-                        ++loop_count;
-                    else if (code[code_ptr] == ']')
-                        --loop_count;
-                }
-            }
-            break;
-
-        case ']':   // end a loop
-            if (tape[ptr] != 0)
-            {
-                int loop_count = 1;
-                while (loop_count > 0)
-                {
-                    --code_ptr;
-                    if (code[code_ptr] == '[')
-                        --loop_count;
-                    else if (code[code_ptr] == ']')
-                        ++loop_count;
-                }
-            }
-            break;
+        cout << "> ";
         
-        default:
-            cout << "\nUnrecognized character: " << code[code_ptr];
-            break;
+        string code;
+        getline(cin, code);
+
+        while (code_ptr < code.size())
+        {
+            char instruction = code[code_ptr];
+
+            // brainfuck instructions
+            // https://en.wikipedia.org/wiki/Brainfuck#Language_design
+            switch (instruction)
+            {
+            case '>':   // increment cell pointer
+                ++ptr;
+                break;
+
+            case '<':   // decrement cell pointer
+                --ptr;
+                break;
+
+            case '+':   // increment byte at ptr cell
+                ++tape[ptr];
+                break;
+
+            case '-':   // decrement byte at ptr cell
+                --tape[ptr];
+                break;
+
+            case '.':   // print byte at ptr cell
+                cout << (char)tape[ptr];
+                break;
+
+            case ',':   // accept one byte of input, store it's value at ptr cell
+                tape[ptr] = cin.get();
+                break;
+
+            case '[':   // start a loop
+                if (tape[ptr] == 0)
+                {
+                    int loop_count = 1;
+                    while (loop_count > 0)
+                    {
+                        ++code_ptr;
+                        if (code[code_ptr] == '[')
+                            ++loop_count;
+                        else if (code[code_ptr] == ']')
+                            --loop_count;
+                    }
+                }
+                break;
+
+            case ']':   // end a loop
+                if (tape[ptr] != 0)
+                {
+                    int loop_count = 1;
+                    while (loop_count > 0)
+                    {
+                        --code_ptr;
+                        if (code[code_ptr] == '[')
+                            --loop_count;
+                        else if (code[code_ptr] == ']')
+                            ++loop_count;
+                    }
+                }
+                break;
+            
+            // for closing the program
+            case 'c':
+                return 0;
+                break;
+            
+            default:
+                cout << "\nUnrecognized character: " << code[code_ptr];
+                break;
+            }
+
+            ++code_ptr;
         }
 
-        ++code_ptr;
+        cout << endl;
     }
-
-    cout << endl;
 
     return 0;
 }
